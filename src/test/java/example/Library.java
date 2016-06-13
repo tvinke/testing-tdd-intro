77eca7ef680b3b9327c2e1759411f82ca1f57603
Ted Vinke <tedvinke@gmail.com>
2016-06-13 17:42:11 +0200

    implemented skeleton method with Javadoc

--- src/test/java/example/Library.java
+++ src/test/java/example/Library.java
@@ -2,4 +2,13 @@ package example;
 
 public class Library {
 
+	/**
+	 * Registers a new member using provided name.
+	 * 
+	 * @param name
+	 *            The name of the member
+	 */
+	public void registerMember(String name) {
+	}
+
 }
